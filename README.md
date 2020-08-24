ad38ff37c7463dae6915618deb65a6316b45dc87
taj0023 <thengakola420@gmail.com>
2020-08-24 15:54:26 +0530

    First commit 4 this project

---
+++ README.md
@@ -0,0 +1,31 @@
+# **Zipfile Bruteforcer (Dictionary)**
+
+
+
+
+### Description:-
+#### A simple Python ZIP-file cracker. 
+#### The first tool in EtTu series.(Pretty sure you know how the name came :) )
+
+
+
+___
+### pros and cons
+* Pros:
+    * Color highlighting.
+    * Simple to use.
+    * Simple to pronounce. (pronounced as 'a to z')
+* Cons:
+    * No Autotab completion. (For now.)
+    * The name is hard to write.(no problem if ur using zsh)
+
+
+
+___
+Usage:
+```bash
+git clone https://github.com/taj0023/EtTuZ.git
+cd EtTuZ
+python3 ettuz.py 
+```
+
---
+++ ettuz.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import zipfile
+import sys
+from tqdm import tqdm
+import time
+import subprocess
+
+
+
+try:
+	thenga = sys.argv[1]
+	if thenga == '-h':
+		print("Usage: python3 zipcrack.py")
+		exit()
+except ValueError:
+	pass
+except IndexError:
+	pass
+
+
+actualzip = input("\033[93mEnter Name(or path) of zipfile to crack: ")
+passlist =  input("\033[93mEnter Name(or path) of dictionary file : ")
+
+numofwords = len(list(open(passlist, "rb")))
+count = 0
+
+with open(passlist,'rb') as passfile:
+	words = passfile.readlines()
+	for password in words:
+		password = password.strip()
+
+		try:
+			with zipfile.ZipFile(actualzip,'r') as my_zip:
+				my_zip.extractall('extracted',pwd=password)
+				print("\033[1;32m-----------------------------------------------")
+				print("       Password Found: --> " + password.decode('utf-8'))
+				print("-----------------------------------------------")
+				break
+		except:
+			print('\033[1;31m[{}/{}]  trying: '.format(count, numofwords) + str(password.decode('utf-8')))
+			count += 1
+			time.sleep(0.0001)
+
+
+
+
+			
